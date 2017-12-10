9842007d410599c27852a138024cf11a5d4197cc
someone <1569275656@qq.com>
2017-12-10 22:08:36 +0800

    websocket -> 添加注释

--- jpa/src/main/java/com/closer/ws/config/WebSocketConfig.java
+++ jpa/src/main/java/com/closer/ws/config/WebSocketConfig.java
@@ -15,13 +15,18 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
+        // 客户端注册连接的端点
         stompEndpointRegistry.addEndpoint("/wsEndPoint").withSockJS();
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
+        // 设置服务器广播消息的基础路径 ( destination 不以这里的为前缀时，消息接收不到 )
         registry.enableSimpleBroker("/topic", "/queue");
-        registry.setUserDestinationPrefix("/user");
+        // 客户端发送消息的前缀
         registry.setApplicationDestinationPrefixes("/app");
+        // 订阅 SendToUser 消息的前缀
+        // ( 客户端订阅 SendToUser 消息时，以 /user 为前缀，加上 destination--SendToUser的value )
+        registry.setUserDestinationPrefix("/user");
     }
 }
